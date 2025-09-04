add60b34911e2bdec13325361ec3788c227b8233
Nitin Kumar <kumar.nk.nitin77@gmail.com>
2025-09-04 14:07:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "myterraformprobucket"      
+    key            = "networking/vpc.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
   
@@ -6,10 +24,10 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
   
-  aws_region          = var.aws_region
-  aws_vpc_cidr        = var.aws_vpc_cidr
-  public_subnet_cidrs = var.public_subnet_cidrs
-  private_subnet_cidrs= var.private_subnet_cidrs
-  db_subnet_cidr      = var.db_subnet_cidr
-  azs                 = var.azs
+  aws_region          = "us-east-1"
+  aws_vpc_cidr        = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24"]
+  private_subnet_cidrs= ["10.0.2.0/24", "10.0.3.0/24"]
+  db_subnet_cidr      = "10.0.4.0/26"
+  azs                 = ["us-east-1a", "us-east-1b"]
 }
